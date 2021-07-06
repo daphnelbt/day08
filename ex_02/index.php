7993df52182f0877d371e13e645034e3c2fafb1b
Daphné Lambret <daphnelambret@MacBook-Air-de-Daphne.local>
2021-07-06 17:26:28 +0200

    add ex_02

--- ex_02/index.php
+++ ex_02/index.php
@@ -1,9 +1,9 @@
 <?php
 $name = NULL;
-if (!isset($name) !=NULL)
-$name = $_GET["name"];
+if (isset($_GET["name"]))
+    $name = $_GET["name"];
 if($name) {
-    echo "Hello" .$name;
+    echo "Hello " .$name;
 }
 else{
     echo "Hello platypus";
